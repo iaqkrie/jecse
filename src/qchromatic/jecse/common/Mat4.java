0a78b3a06d1eff23d6e5e8e495c3e4615fc580fc
iaqkrie <99099793+iaqkrie@users.noreply.github.com>
2025-11-13 00:31:50 +0300

    251113a

--- src/qchromatic/jecse/common/Mat4.java
+++ src/qchromatic/jecse/common/Mat4.java
@@ -3,13 +3,23 @@ package qchromatic.jecse.common;
 public class Mat4 {
 	private float[] _matrix;
 
-	public Mat4 (float[] matrix) { _matrix = matrix.clone(); }
-	public Mat4 () { _matrix = new float[16]; }
+	// region ctors
+	public Mat4 (float[] matrix) {
+		_matrix = matrix.clone();
+	}
+
+	public Mat4 () {
+		_matrix = new float[16];
+		identity();
+	}
+	// endregion
 
 	public float get (int x, int y) { return _matrix[y * 4 + x]; }
 	public void set (int x, int y, float value) { _matrix[y * 4 + x] = value; }
 
 	public Mat4 identity () {
+		for (int i = 0; i < 16; i++) _matrix[i] = 0f;
+
 		set(0, 0, 1f);
 		set(1, 1, 1f);
 		set(2, 2, 1f);
@@ -35,12 +45,35 @@ public class Mat4 {
 		return this;
 	}
 
+	public static Mat4 translation (Vec3 position) {
+		return translation(position.x, position.y, position.z);
+	}
 	public static Mat4 translation (float x, float y, float z) {
 		Mat4 result = new Mat4();
-		result.identity();
 		result.set(3, 0, x);
 		result.set(3, 1, y);
 		result.set(3, 2, z);
+		return result;
+	}
+
+	public static Mat4 rotation (Vec3 rotation) {
+		return rotation(rotation.x, rotation.y, rotation.z);
+	}
+	public static Mat4 rotation (float x, float y, float z) {
+		return rotation(Quaternion.euler(x, y, z));
+	}
+	public static Mat4 rotation (Quaternion rotation) {
+		return rotation.toMatrix();
+	}
+
+	public static Mat4 scale (Vec3 scale) {
+		return scale(scale.x, scale.y, scale.z);
+	}
+	public static Mat4 scale (float x, float y, float z) {
+		Mat4 result = new Mat4();
+		result.set(0, 0, x);
+		result.set(1, 1, y);
+		result.set(2, 2, z);
 		return result;
 	}
 }
--- src/qchromatic/jecse/component/TransformComponent.java
+++ src/qchromatic/jecse/component/TransformComponent.java
@@ -1,20 +1,37 @@
 package qchromatic.jecse.component;
 
+import qchromatic.jecse.common.Mat4;
+import qchromatic.jecse.common.Quaternion;
 import qchromatic.jecse.common.Vec3;
 import qchromatic.jecse.core.Component;
 
 public class TransformComponent extends Component {
 	private Vec3 _position;
+	private Quaternion _rotation;
+	private Vec3 _scale;
+
+	private Mat4 _modelMatrix;
 
 	// region ctors
 	public TransformComponent (TransformComponent other) {
 		if (other == null) return;
 
 		_position = new Vec3(other._position);
+		_rotation = new Quaternion(other._rotation);
+		_scale = new Vec3(other._scale);
+
+		_modelMatrix = new Mat4().identity();
 	}
 
 	public TransformComponent () {
-		_position = new Vec3();
+		this(new Vec3(), new Quaternion(), new Vec3(1f));
+	}
+	public TransformComponent (Vec3 position, Quaternion rotation, Vec3 scale) {
+		_position = new Vec3(position);
+		_rotation = new Quaternion(rotation);
+		_scale = new Vec3(scale);
+
+		_modelMatrix = new Mat4().identity();
 	}
 	// endregion
 
@@ -22,5 +39,30 @@ public class TransformComponent extends Component {
 	public TransformComponent position (Vec3 position) {
 		_position = new Vec3(position);
 		return this;
+	}
+
+	public Quaternion rotation () { return new Quaternion(_rotation); }
+	public TransformComponent rotation (Quaternion rotation) {
+		_rotation = new Quaternion(rotation);
+		return this;
+	}
+
+	public Vec3 scale () { return new Vec3(_scale); }
+	public TransformComponent scale (Vec3 scale) {
+		_scale = new Vec3(scale);
+		return this;
+	}
+
+	public Mat4 getModelMatrix () {
+		return _modelMatrix;
+	}
+
+	private void updateModelMatrix () {
+		_modelMatrix.identity();
+
+		_modelMatrix
+				.mul(Mat4.translation(_position))
+				.mul(Mat4.rotation(_rotation))
+				.mul(Mat4.scale(_scale));
 	}
 }
